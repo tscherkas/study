9fa651e34c352743b108aa361b7b05a360cbea17
tscherkas <tscherkas.it@gmail.com>
2016-02-14 22:41:54 +0300

    Expressions lab

---
+++ TPINTELS/lab1/lab1/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.VariableColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ValueColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(93, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Выражение 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 26);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(93, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Выражение 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(3, 49);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(93, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.Text = "Выражение 3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(3, 72);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(93, 17);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.Text = "Выражение 4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(3, 95);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(93, 17);
+            this.radioButton5.TabIndex = 5;
+            this.radioButton5.Text = "Выражение 5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 118);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(105, 42);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.VariableColumn,
+            this.ValueColumn});
+            this.dataGridView1.Location = new System.Drawing.Point(114, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(250, 109);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // VariableColumn
+            // 
+            this.VariableColumn.HeaderText = "Переменная";
+            this.VariableColumn.Name = "VariableColumn";
+            this.VariableColumn.ReadOnly = true;
+            // 
+            // ValueColumn
+            // 
+            this.ValueColumn.HeaderText = "Значение";
+            this.ValueColumn.Name = "ValueColumn";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.radioButton1);
+            this.flowLayoutPanel1.Controls.Add(this.radioButton2);
+            this.flowLayoutPanel1.Controls.Add(this.radioButton3);
+            this.flowLayoutPanel1.Controls.Add(this.radioButton4);
+            this.flowLayoutPanel1.Controls.Add(this.radioButton5);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.dataGridView1);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.textBox1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(607, 284);
+            this.flowLayoutPanel1.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(114, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 26);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(114, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Результат";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 284);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn VariableColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ValueColumn;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TPINTELS/lab1/lab1/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        public List<Expression> expressions { get; }
+        public Expression currentExpression { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            expressions = new List<Expression>();
+            expressions.Add(new Expression1());
+            expressions.Add(new Expression2());
+            expressions.Add(new Expression3());
+            expressions.Add(new Expression4());
+            expressions.Add(new Expression5());
+            radioButton1_CheckedChanged(null, null);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                pictureBox1.Image = lab1.Properties.Resources._1;
+                currentExpression = expressions[0];
+                dataGridView1.Rows.Clear();
+                dataGridView1.Rows.Add(2);
+                dataGridView1.Rows[0].Cells[0].Value = "x";
+                dataGridView1.Rows[1].Cells[0].Value = "y";
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                pictureBox1.Image = lab1.Properties.Resources._2;
+                currentExpression = expressions[1];
+                dataGridView1.Rows.Clear();
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[0].Cells[0].Value = "x";
+            }
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                pictureBox1.Image = lab1.Properties.Resources._3;
+                currentExpression = expressions[2];
+                dataGridView1.Rows.Clear();
+                dataGridView1.Rows.Add(2);
+                dataGridView1.Rows[0].Cells[0].Value = "x";
+                dataGridView1.Rows[1].Cells[0].Value = "y";
+
+            }
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked)
+            {
+                pictureBox1.Image = lab1.Properties.Resources._4;
+                currentExpression = expressions[3];
+                dataGridView1.Rows.Clear();
+                dataGridView1.Rows.Add(2);
+                dataGridView1.Rows[0].Cells[0].Value = "x";
+                dataGridView1.Rows[1].Cells[0].Value = "y";
+
+            }
+
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton5.Checked)
+            {
+                pictureBox1.Image = lab1.Properties.Resources._5;
+                currentExpression = expressions[4];
+                dataGridView1.Rows.Clear();
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[0].Cells[0].Value = "x";
+
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            List<double> expressionParams = new List<double>();
+            foreach (DataGridViewRow item in dataGridView1.Rows)
+            { 
+                double p;
+                if(Double.TryParse((string)item.Cells[1].Value,out p))
+                    expressionParams.Add(p);
+                else
+                {
+                    textBox1.Text = "Параметр "+ item.Cells[0].Value + " задан неверно";
+                    break;
+                }
+            }
+            if (textBox1.Text.Length == 0)
+                textBox1.Text =  currentExpression.calculate(expressionParams).ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public abstract class Expression
+    {
+        public abstract double calculate(List<double> paramsArray);
+    }
+    public class Expression1 : Expression
+    {
+        public override double calculate(List<double> paramsArray)
+        {
+            double x = paramsArray[0];
+            double y = paramsArray[1];
+            return (3 + Math.Exp(y-1))/(1+ x*x*Math.Abs(y-Math.Tan(x)));
+        }
+    }
+    public class Expression2 : Expression
+    {
+        public override double calculate(List<double> paramsArray)
+        {
+            double x = paramsArray[0];
+            return x-x*x*x/3 + Math.Pow(x,5)/5;
+        }
+    }
+    public class Expression3 : Expression
+    {
+        public override double calculate(List<double> paramsArray)
+        {
+            double x = paramsArray[0];
+            double y = paramsArray[1];
+            return 
+                Math.Log(
+                    Math.Abs(
+                        (y-Math.Sqrt(
+                            Math.Abs(x)))
+                        *
+                        (x - y/(x+x*x/4))
+                        )
+                   );
+        }
+    }
+    public class Expression4 : Expression
+    {
+        public override double calculate(List<double> paramsArray)
+        {
+            double x = paramsArray[0];
+            double y = paramsArray[1];
+            return
+               Math.Pow(1 - Math.Tan(x), 1 / Math.Tan(x))
+               +
+               Math.Cos(x - y);
+        }
+    }
+    public class Expression5 : Expression
+    {
+        public override double calculate(List<double> paramsArray)
+        {
+            double x = paramsArray[0];
+            return
+               Math.Log(
+                   Math.Abs(
+                       Math.Cos(x)
+                       )
+                   )
+              /
+              Math.Log(1+x*x);
+        }
+    }
+}
